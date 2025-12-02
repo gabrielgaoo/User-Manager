a5faefcdcaf6db1fd6e0a44c52dc0d351f54f483
Gabriel Guimaraes <gabrielplug32@gmail.com>
2025-12-01 21:51:22 -0300

    Refatorando a classe usuario

--- UserManager/src/main/java/com/mycompany/usermanager/Model/Usuario.java
+++ UserManager/src/main/java/com/mycompany/usermanager/Model/Usuario.java
@@ -4,31 +4,116 @@
  */
 package com.mycompany.usermanager.Model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Lenovo
  */
 public class Usuario {
-            private String nome;
-            private String senha;
-            private Status status;
-            
-            public Usuario(String nome, String senha,Status status){
-                        this.nome = nome;
-                        this.senha = senha;            
-                        this.status = status.PENDENTE;
-            }
+    private Integer id;
+    private String nomeCompleto;
+    private String nomeUsuario;
+    private String senha;
+    private boolean isAdministrador;
+    private boolean isPrimeiroAdmin;
+    private LocalDateTime dataCadastro;
+    private boolean autorizado;
+    
+    public Usuario(String nomeCompleto, String nomeUsuario, String senha, boolean isAdministrador, boolean isPrimeiroAdmin) {
+        this.nomeCompleto = nomeCompleto;
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+        this.isAdministrador = isAdministrador;
+        this.isPrimeiroAdmin = isPrimeiroAdmin;
+        this.dataCadastro = LocalDateTime.now();
+        this.autorizado = isPrimeiroAdmin;
+    }
+    
+    public Usuario(Integer id, String nomeCompleto, String nomeUsuario, String senha, 
+                   boolean isAdministrador, boolean isPrimeiroAdmin, 
+                   LocalDateTime dataCadastro, boolean autorizado) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+        this.isAdministrador = isAdministrador;
+        this.isPrimeiroAdmin = isPrimeiroAdmin;
+        this.dataCadastro = dataCadastro;
+        this.autorizado = autorizado;
+    }
 
-            public String getNome() {
-                return nome;
-            }
+    public Integer getId() {
+        return id;
+    }
 
-            public String getSenha() {
-                return senha;
-            }     
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-            public Status getStatus() {
-                return status;
-            }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAdministrador() {
+        return isAdministrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        isAdministrador = administrador;
+    }
+
+    public boolean isPrimeiroAdmin() {
+        return isPrimeiroAdmin;
+    }
+
+    public void setPrimeiroAdmin(boolean primeiroAdmin) {
+        isPrimeiroAdmin = primeiroAdmin;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public boolean isAutorizado() {
+        return autorizado;
+    }
+
+    public void setAutorizado(boolean autorizado) {
+        this.autorizado = autorizado;
+    }
+    
+     public boolean validaUsuarioAtivo() {
+        return this.autorizado;
+    }
+     
+    public boolean validaUsuarioPendente() {
+        return !this.autorizado && !this.isPrimeiroAdmin;
+    }
+
             
 }
